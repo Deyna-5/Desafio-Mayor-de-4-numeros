86454bafc3bcf8dd358e7692a0e89b3cdb295b8b
Deyna Contreras <deyna.alexac@gmail.com>
2021-01-10 19:48:11 -0300

    Primer commit desafio: Mayor de 4 numero

---
+++ mayor_de_4.rb
@@ -0,0 +1,20 @@
+num1 = ARGV[0].to_i
+num2 = ARGV[1].to_i
+num3 = ARGV[2].to_i
+num4 = ARGV[3].to_i
+
+if 	num1 >= num2 && num1 >= num3 && num1 >= num4
+	puts num1
+else
+	if num2 > num1 && num2 >= num3 && num2 >= num4
+		puts num2
+	else
+		if num3 > num1 && num3 > num2 && num3 >= num4
+			puts num3
+		else
+			if num4 > num1 && num4 > num2 && num4 > num3
+			puts num4
+			end	
+		end
+	end
+end
